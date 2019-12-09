594685409236c4084b912437440892833dd84ef8
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2019-12-08 19:35:35 -0800

    Initial commit

---
+++ Path Tracer/Path Tracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Path Tracer
+//
+//  Created by Jeremy Berchtold on 12/8/19.
+//  Copyright © 2019 Jeremy Berchtold. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
