7e275787b05728541c4efd21f2aaae4e48a6ccb4
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2020-10-18 23:07:25 -0700

    Added GoogleTest to CMake

---
+++ test/StringUtil-test.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "StringUtil.hpp"
+
+TEST(StringUtil, test1) {
+    //arrange
+    std::vector<std::string> v;
+    v.push_back("a");
+    v.push_back("hi");
+    v.push_back("ba%% 32");
+    v.push_back(" def");
+    //act
+    auto out = stringutil::split("a,hi,ba%% 32, def", ',');
+    //assert
+    EXPECT_EQ(out.size(), v.size());
+    for (int i = 0; i < v.size(); i++) {
+        EXPECT_EQ(out[i], v[i]);
+    }
+}
